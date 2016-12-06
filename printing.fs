8dd20f25b4473b2aeda3d03fea847634f959d0a3
Brendan Fahy <bmfahy@gmail.com>
2016-12-06 13:48:14 +0900

    added some helper print functions (used in debugging)

--- printing.fs
+++ printing.fs
@@ -5,6 +5,15 @@ let rec plist lst =
         | head :: tail -> printf "%d " head; plist tail
         | [] -> printfn "\n"
 
-let labellist s lst = printf "%s"; plist lst
+let printn n lst =
+    List.toSeq lst |> Seq.truncate n |> Seq.iter (printf "%A ") ; printfn ""
 
-let print = printfn "%d"
+let print10 lst = printn 10 lst
+
+let lprintn label n lst =
+    printf "%s: " label; printn n lst
+
+let lprint10 label lst =
+    lprintn label 10 lst
+
+let lpd label d = printf "%s: %d" label d 
