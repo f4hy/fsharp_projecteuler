6eba98cb7136389a79906c703274723d2233674d
Brendan Fahy <bmfahy@gmail.com>
2016-12-06 10:44:27 +0900

    remove underscores in definitions which is bad style

--- p1.fsx
+++ p1.fsx
@@ -1,12 +1,12 @@
 // First attempt
 let under1000 = [ 1..999 ]
 
-let div_by_3_or_5 x =
+let divby3or5 x =
     if x % 5 = 0 then true
     elif x % 3 = 0 then true
     else false
 
-let l = List.filter div_by_3_or_5 under1000
+let l = List.filter divby3or5 under1000
 
 
 let plist = List.iter (fun x -> printf "%d " x)
@@ -20,10 +20,8 @@ printfn "\nAnswer: %d" answer
 // Now lets try to improve that
 
 // Simpler div by fuction
-let div_by_3_or_5p x = (x % 5 = 0) || (x % 3 = 0)
+let divby3or5p x = (x % 5 = 0) || (x % 3 = 0)
 // make the filter a function
-let filter3or5 = List.filter div_by_3_or_5p
+let filter3or5 = List.filter divby3or5p
 // Pipe the functions for the answer
 under1000 |> filter3or5 |> List.sum |> printfn "\nAnswer: %d"
-
-
