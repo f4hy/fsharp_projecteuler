e82f4a4c205838a465d474ca314bfa7a5490cff8
Brendan Fahy <bmfahy@gmail.com>
2016-12-07 16:36:05 +0900

    minor cleanup of p4

--- p4.fsx
+++ p4.fsx
@@ -1,7 +1,7 @@
 // Euler problem #4
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-// 
-// Find the largest palindrome made from the product of two 3-digit numbers 
+//
+// Find the largest palindrome made from the product of two 3-digit numbers
 
 // First thought is to just find the range between 111*111 and
 // 999*999, and count down til we find a planedrome
@@ -12,19 +12,16 @@
 // To check palendroms we need to reverse strings
 let Reverse (s:string) =
     new string(Array.rev (s.ToCharArray()))
-    
+
 // Use string reversal to check palindromes
 let IsPalindrome x =
     let s = string x
     let n = s.Length
-    if s = Reverse s then
-        true
-    else
-        false    
-    
-// go through all 3 digit numbers and find all the palindromes
-let all_3x3_pals = [ for i in 100..999 do for j in i..999 do if IsPalindrome (i*j) then yield i*j ]
+    s = Reverse s
 
-printfn "Found %d palindromes" all_3x3_pals.Length
-    
-printfn "Answer: %d " (List.max all_3x3_pals)
+// go through all 3 digit numbers and find all the palindromes
+let all3x3pals = [ for i in 100..999 do for j in i..999 do if IsPalindrome (i*j) then yield i*j ]
+
+printfn "Found %d palindromes" all3x3pals.Length
+
+printfn "Answer: %d " (List.max all3x3pals)
